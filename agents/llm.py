d0fb3be5ae3d26d3ef0cce5b1a860960d5b52922
mohamed hassan <mohamed6722529@gmail.com>
2026-09-07 16:14:44 +0300

    update code and docker config

--- agents/llm.py
+++ agents/llm.py
@@ -1,8 +1,16 @@
-from langchain_ollama import ChatOllama
+import os 
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
 from agents.tools import search, calculator, get_weather
 
+load_dotenv()
 
-llm = ChatOllama(model='llama3.1')
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    google_api_key=os.getenv("GEMINAI_KEY"),
+    temperature=0.2,
+    max_output_tokens=512
+)
 
 tools = [search, calculator, get_weather]
 
