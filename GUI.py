332ea0e7664c1158f72de53ec76c0a06b3e72862
mohamed hassan <mohamed6722529@gmail.com>
2026-09-08 14:06:33 +0300

    update code and docker config

--- GUI.py
+++ GUI.py
@@ -25,8 +25,8 @@ if user_input:
         response = app.invoke({"messages":[("human",user_input)]},
                               config={"configurable": {"thread_id": "streamlit_user"}})
         
-        assistant_response = response["messages"][-1].content["text"]
-        st.markdown(assistant_response)
+        assistant_response = response["messages"][-1].content
+        st.markdown(assistant_response[1]['text'])
 
         st.session_state.messages.append({"role":"assistant",
                                           "content":assistant_response})
