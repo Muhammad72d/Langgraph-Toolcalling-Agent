0508a626fedc9a57167d2e16b5818b6a8f672f9a
mohamed hassan <mohamed6722529@gmail.com>
2026-09-08 14:14:43 +0300

    update code and docker config

--- agents/agent.py
+++ agents/agent.py
@@ -5,7 +5,7 @@ def agent (state:state) -> dict:
     """ this is the main agent which will take the query form the user and take the decistion to use a too or automatically answer"""
     try:
         response = llm_with_tools.invoke(state["messages"])
-        return {"messages":[response].content[0]["text"]}
+        return {"messages":[response.content[0]["text"]]}
     except Exception as e:
         return {"messages":[("ai",f"there is an error while processing the query :{str(e)}")]}
 
