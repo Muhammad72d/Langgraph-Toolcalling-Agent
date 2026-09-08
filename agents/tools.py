b554a6b5c95be24e858b41da64b239d1c189ce18
mohamed hassan <mohamed6722529@gmail.com>
2026-09-08 14:38:45 +0300

    update code and docker config

--- agents/tools.py
+++ agents/tools.py
@@ -54,7 +54,7 @@ def get_weather(location:str) -> str:
     params = {
         "latitude": latitude,
         "longitude": longitude,
-        "current": True,
+        "current": "temperature_2m,wind_speed_10m,wind_direction_10m",
         "timezone": timezone
     }
     try:
